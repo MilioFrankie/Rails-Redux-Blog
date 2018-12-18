71489bc80d07b93663a5f9c868e3d97c0fece9e9
Emilio <em.francomartinez@gmail.com>
2018-12-18 15:58:47 -0700

    created blogs controller, add style to app

--- app/controllers/api/blogs_controller.rb
+++ app/controllers/api/blogs_controller.rb
@@ -1,16 +1,42 @@
 class Api::BlogsController < ApplicationController
   def index
+    render json: Blog.all 
   end
 
   def show
+    render json: @blog
   end
 
   def create
+    blog = Blog.create(blog_params)
+    if blog.save
+      render json: blog
+    else
+      render json: { errors: blog.errors.full_messages.join(',') }, status: 422
+    end
   end
 
   def update
+    @blog.update(blog_params)
+    if @blog.save
+      render json: @blog
+    else
+      render json: { errors: @blog.errors.full_messages.join(',') }, status: 422
+    end
   end
 
   def destroy
+    @blog.destroy
   end
+
+  private
+
+  def set_blog 
+  @blog = Blog.find(params[:id])
+  end
+
+  def blog_params
+  params.require(:blog).permit(:title, :body, :author)
+  end
+
 end
--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import React, { Fragment } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Blog from "./components/Blog";
+import { Container } from "semantic-ui-react";
 
 const App = () => (
   <Fragment>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/blog/:name" component={Blog} />
-    </Switch>
+    <Container>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/blog/:name" component={Blog} />
+      </Switch>
+    </Container>
   </Fragment>
 );
 
